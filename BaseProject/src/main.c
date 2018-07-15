7b2ab13be8d1b890199818f5defc75c25ce235e1
Keyarash Ghiasi <kghiasi@student.ethz.ch>
2018-07-16 00:52:29 +0200

    connection to Sara done, command sending function complete

--- BaseProject/src/main.c
+++ BaseProject/src/main.c
@@ -32,8 +32,9 @@
 #include "conf_uart_serial.h"
 #include "LTE_interface.h"
 
-#define USART_FUNCTION 0 
-#define LED_FUNCTION 1
+#define USART_FUNCTION 1 
+#define LED_FUNCTION 0
+#define SPI_FUNCTION 0
 
 #define ioport_set_pin_peripheral_mode(pin, mode) \
 do {\
@@ -45,25 +46,50 @@ do {\
 /* usart receive interrupt handler */
 void CONF_UART_ISR_HANDLER(void)
 {
+
 	uint32_t dw_status = usart_get_status(CONF_UART);
 	if (dw_status & US_CSR_RXRDY) {
 		uint32_t received_byte;
+		
 		usart_read(CONF_UART, &received_byte);
-		usart_write_line(CONF_UART, "HELLO");
+		usart_write_line(USART_SERIAL, "HELLO");
 	}
 }
 
+/* USART to arduino handler */ 
+void USART_SERIAL_ISR_HANDLER(void)
+{
+	char b [50]; 
+	uint32_t dw_status = usart_get_status(USART_SERIAL);
+	if (dw_status & US_CSR_RXRDY) {
+		//uint32_t received_byte;
+		char received_byte;
+		usart_read(USART_SERIAL, &received_byte);
+		//usart_write_line(CONF_UART, "rec from ard");
+		sprintf(b , "%c" , received_byte); 
+		usart_write_line(CONF_UART, b );
+	}
+}
+
+void saraWriteCommand(const char * stringInput){	
+	uint8_t byte[1];
+	byte[0] = 0x0D;
+	usart_write_line(USART_SERIAL, stringInput );
+	usart_serial_write_packet(USART_SERIAL , byte , 1);
+	delay_ms(1000); 
+}
 
 
 int main (void)
 {
+	int one_time_run = 0; 
 	
 	/* Insert system clock initialization code here (sysclk_init()). */
 	sysclk_init(); 
 	board_init();
 	ioport_init(); 
 
-	//ioport_set_port_dir(LED_PORT , LED_MASK ,	IOPORT_DIR_OUTPUT);
+	ioport_set_port_dir(LED_PORT , LED_MASK ,	IOPORT_DIR_OUTPUT);
 	 
 	ioport_set_pin_peripheral_mode(COM_PORT_RX_PIN, IOPORT_MODE_MUX_A);
 	ioport_set_pin_peripheral_mode(COM_PORT_TX_PIN, IOPORT_MODE_MUX_A);
@@ -97,16 +123,41 @@ int main (void)
 	 usart_enable_interrupt(CONF_UART,US_IER_RXRDY);
 	 /* turning usart interrupt on (nested vector interrupt controller */
 	 NVIC_EnableIRQ(CONF_UART_IRQn);
+	 /* setting the type of usart interrupt: in this case receive ready*/
+	 usart_enable_interrupt(USART_SERIAL,US_IER_RXRDY);
+	 /* turning usart interrupt on (nested vector interrupt controller */
+	 NVIC_EnableIRQ(USART_SERIAL_IRQn);
+	 
 	while (1)
 	{
-	    #if USART_FUNCTION
+		#if USART_FUNCTION
 		uint8_t received_byte = 0 ; 
 		int a = sysclk_get_cpu_hz();
 		char b [50]; 
+	    
+		  
+		
 		sprintf(b , "%d\n" , a);  
-		delay_ms(200);
-		   	
-		//usart_write_line(CONF_UART , b); 
+		//usart_write_line(USART_SERIAL, "AT+CGMR" );
+		//usart_serial_write_packet(USART_SERIAL , byte , 1); 
+		//delay_ms(1000);
+		
+		
+		saraWriteCommand("AT+CGMR"); 
+		
+		if(one_time_run==0){
+			one_time_run = 1; 
+			saraWriteCommand("AT+CMEE=2"); 
+		}
+		
+		saraWriteCommand("AT+CPIN?");   
+		saraWriteCommand("AT+URAT?");  // getting Radio Access Technology, for SARA is 7, 8 = LTE CAT M1/B
+		saraWriteCommand("AT+CFUN?");  // getting module functionality
+		saraWriteCommand("AT+COPS?");  // getting network registration mode: 0 for automatic 
+		saraWriteCommand("AT+CEREG?"); 
+		
+		
+		//usart_write_line(CONF_UART , "trans"); 
 		
 		/* sample project of io init*/
 		//ioport_toggle_port_level(LED_PORT, LED_MASK);
@@ -116,10 +167,23 @@ int main (void)
 		
 		/* put this part of USART receive into an interrupt service routine */		
 		//usart_write_line(CONF_UART , "hello"); 		
-		usart_write_line(USART_SERIAL , "to arduino"); 			
+		//usart_write_line(USART_SERIAL , "to arduino"); 			
+		#endif
+	
+		#if LED_FUNCTION
+			
+		ioport_toggle_port_level(LED_PORT, LED_MASK);
+		delay_ms(100); 
+		
 		#endif
 		
 		
+		#if SPI_FUNCTION
+		
+		
+		
+		
+		#endif 
 		
 		
 	}
